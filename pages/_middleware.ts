3ff11bdec02607c6e36b879faf1dd44136fde360
t-k-satoh <syakuyanaoto@gmail.com>
2021-12-13 00:40:36 +0900

    chore(env): fix VERCEL_ENV

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,5 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+// VERCEL_ENV
+// The Environment that the app is deployed an running on. The value can be either production, preview, or development.
+
 export function middleware(req: NextRequest) {
   if (process.env.VERCEL_ENV === 'production') {
     return NextResponse.next()
@@ -7,7 +10,7 @@ export function middleware(req: NextRequest) {
 
   const basicAuth = req.headers.get('authorization')
 
-  if (basicAuth && process.env.VERCEL_ENV === 'develop') {
+  if (basicAuth && (process.env.VERCEL_ENV === 'development' || process.env.VERCEL_ENV === 'preview')) {
     const auth = basicAuth.split(' ')[1]
     const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
 
