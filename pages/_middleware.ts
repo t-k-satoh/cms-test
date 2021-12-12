2af3d361758074c8d2b07c0c8235e90eecdd798b
t-k-satoh <syakuyanaoto@gmail.com>
2021-12-12 23:49:23 +0900

    chore(app): add basic auth

---
+++ pages/_middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  if (process.env.VERCEL_ENV === 'production') {
+    return NextResponse.next()
+  }
+
+  const basicAuth = req.headers.get('authorization')
+
+  if (basicAuth && process.env.VERCEL_ENV === 'develop') {
+    const auth = basicAuth.split(' ')[1]
+    const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':')
+
+    if (user === process.env.BASIC_AUTH_USER && pwd === process.env.BASIC_AUTH_PASSWORD) {
+      return NextResponse.next()
+    }
+  }
+
+  return new Response('Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
